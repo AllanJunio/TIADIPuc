317f35bcd8da0a76d83081e0c519efb244fad2b8
AllanJunio <allanjuniomkt@gmail.com>
2025-04-21 23:31:06 -0300

    Correção git

--- project/obj/Debug/net8.0/YourProject.AssemblyInfo.cs
+++ project/obj/Debug/net8.0/YourProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YourProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1500b8a4d5c05e60e2d22d1aeab9a49bc40138e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c918063150789ab8ee1ba080cb978f5172a4f211")]
 [assembly: System.Reflection.AssemblyProductAttribute("YourProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YourProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
